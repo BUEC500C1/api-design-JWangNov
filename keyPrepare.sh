27706d1502a21d9c1d582a492d43123973a1d25c
JWangNov <wangjialun@126.com>
2020-02-02 23:58:07 -0500

    Create keyPrepare.sh

---
+++ keyPrepare.sh
@@ -0,0 +1,14 @@
+echo "Generating \"myOpenWeatherKey.py\" file..."
+
+echo " This returns my OpenWeather API key in string format
+# Should replace {MY_API_KEY} with my own API key
+
+def myKey():
+    return \"{MY_API_KEY}\"
+    #       ^^^^^^^^^^^^  replace
+" > myOpenWeatherKey.py
+
+echo "
+============================== DONE =============================
+Please open the file and replace {MY_API_KEY} with your API key.
+================================================================="
